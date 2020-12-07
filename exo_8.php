a00b8b878808f02fc2b8f0cb3eea4d30983b84e5
julie-devos <julie.devos@epitech.eu>
2020-12-07 15:23:24 +0100

    initial commit

--- exo_8.php
+++ exo_8.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
+    <title>PhpPart1Exo8</title>
 </head>
 
 <body>
+<?php
+$addition=3+4;
+?>
+<?="Le résultat de 3 plus 4 est égal à "." "."$addition"?>
+<br>
+<?php
+$multiplication=5*20;
+?>
+<?="Le résultat de 5 multiplié par 20 est égal à"." "."$multiplication"?>
+<br>
+<?php
+$division=45/5;
+?>
+<?="Enfin, le résultat de 45 divisé par 5 vaut"." "."$division"?>
 
 </body>
 </html>
